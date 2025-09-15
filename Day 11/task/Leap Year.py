1e51bd45b64fcf80a0cab849d3c2d3c16966fe43
Haseeb <haseebanwer95@gmail.com>
2025-09-16 00:38:46 +0200

    20250916

---
+++ Day 11/task/Leap Year.py
@@ -0,0 +1,22 @@
+
+
+
+def is_leap_year(year):
+
+
+    if year%4==0:
+        if year%100!=0:
+            return True
+        else:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+
+    else:
+        return False
+
+    # Write your code here.
+    # Don't change the function name.
+
+print(is_leap_year(2025))
--- Day 11/task/main.py
+++ Day 11/task/main.py
@@ -1,21 +1,59 @@
+import art
+import random
+def deal_card():
+    """Returns a random card from the deck"""
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
 
 
-def is_leap_year(year):
+total_user=[]
+total_dealer=[]
+
+total_user.append(int(deal_card()))
+total_user.append(int(deal_card()))
+print("User First card:", total_user[0])
+print("User Second card:", total_user[1])
+print("User Total: ", sum(total_user))
+print("---------------------------------------------")
+
+total_dealer.append(int(deal_card()))
+total_dealer.append(int(deal_card()))
+print("Dealer First card:", total_user[0])
+print("---------------------------------------------")
+choice=input("Do you want to hit or stand?: h/s ")
+
+while choice == "h":
+    total_user.append(int(deal_card()))
+    print("---------------------------------------------")
+    print("User new card:",  total_user[-1])
+    print("User Total: ", sum(total_user))
+    print("---------------------------------------------")
+    if sum(total_user)>21:
+        print("Dealer wins!")
+        break
+    choice = input("Do you want to hit or stand?: h/s ")
 
 
-    if year%4==0:
-        if year%100!=0:
-            return True
-        else:
-            if year % 400 == 0:
-                return True
-            else:
-                return False
+print("Dealer Second card:", total_dealer[1])
+print("Dealer Total: ", sum(total_dealer))
+print("---------------------------------------------")
 
+
+print("Total score user: ", sum(total_user))
+print("Total score dealer: ",  sum(total_dealer))
+
+if sum(total_user)>21:
+    print("Dealer win!")
+elif sum(total_dealer)>21:
+    print("User win!")
+else:
+    if sum(total_dealer)>sum(total_user):
+        print("Dealer win!")
     else:
-        return False
+        print("User win!")
 
-    # Write your code here.
-    # Don't change the function name.
 
-print(is_leap_year(2025))
+
+
+

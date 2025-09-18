be77d7d1f89e8aa2fabee987b74edcd062535de9
manwar <haseebanwer95@gmail.com>
2025-09-18 17:18:22 +0200

    20250918

--- Day 13/Describe the Problem/task.py
+++ Day 13/Describe the Problem/task.py
@@ -1,5 +1,6 @@
 def my_function():
     for i in range(1, 20):
+
         if i == 20:
             print("You got it")
 
@@ -8,5 +9,10 @@ my_function()
 
 # Describe the Problem - Write your answers as comments:
 # 1. What is the for loop doing?
+# range 1,20 means it will run from 0 to 19
+
 # 2. When is the function meant to print "You got it"?
+# when i ==20
+
 # 3. What are your assumptions about the value of i?
+# it will start from 0 and
